2d0894626817d9cfd5629c2aeb4d5090471689c6
Nicole Milmine <nicoleamilmine@gmail.com>
2024-02-12 08:48:57 -0500

    Add project files.

---
+++ Controllers/GuestBookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebDevGroupProject.Controllers
+{
+    public class GuestBookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/SearchingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebDevGroupProject.Models
+{
+    public class SearchingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
