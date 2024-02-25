48020895ce1fdf7d5ed50d749541250bcdc868ce
Nicole <122618341+n2milmin@users.noreply.github.com>
2024-02-25 12:33:44 -0500

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Syncfusion.EJ2.Lists;
 using System.Diagnostics;
+using WebDevGroupProject.Data;
 using WebDevGroupProject.Models;
 
 namespace WebDevGroupProject.Controllers
@@ -7,18 +10,59 @@ namespace WebDevGroupProject.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+		private readonly AppDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
-        }
+			_db = context;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			ViewData["ErrMsg"] = "";
+			ViewData["NoInput"] = "";
+			return View();
+		}
 
-        public IActionResult Privacy()
+		[HttpGet("Search/{searchString?}")]
+		public IActionResult Index(string searchString, string sortBy)
+		{
+			ViewData["ErrMsg"] = "";
+			ViewData["NoInput"] = "";
+			@ViewData["searchString"] = searchString;
+
+			if (!string.IsNullOrEmpty(searchString))
+			{
+				if (sortBy == "Flights")
+				{
+					ViewData["Airline"] = true;
+					return RedirectToAction("Index", "Flight", new { searchString });
+				}
+				else if (sortBy == "Hotels")
+				{
+					ViewData["HotelName"] = true;
+					return RedirectToAction("Index", "Hotel", new { searchString });
+				}
+				else if (sortBy == "CarRentals")
+				{
+					ViewData["CompanyName"] = true;
+					return RedirectToAction("Index", "Car_Rentals_", new { searchString });
+				}
+				else
+				{
+					ViewData["ErrMsg"] = "Please specify the search parameters";
+				}
+			}
+
+			ViewData["NoInput"] = "Please enter something to search.";
+			ViewData["Flights"] = false;
+			ViewData["Hotels"] = false;
+			ViewData["CarRentals"] = false;
+			return View();
+		}
+
+		public IActionResult Privacy()
         {
             return View();
         }
